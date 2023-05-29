e0fddd95eb07bcbb832f5a0133c2ed6d1a27a8fb
Cenentury0941 <67197834+cenentury0941@users.noreply.github.com>
2023-05-29 14:27:12 +0530

    stonks

---
+++ src/components/Counter.js
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import './Counter.css';
+import { Button } from "@mui/material";
+import stonks from "../images/stonks.png";
+import notstonks from "../images/notstonks.png";
+
+export function Counter()
+{
+
+    const [count, setcount] = useState(0);
+    const back = React.useRef(null);
+    const [img, setimg] = useState( 'url('+require("../images/default.png")+')' );
+    
+    function increase()
+    {
+        let new_count = count + 1;
+        new_count = new_count < 0 ? 0 : ( new_count > 10 ? 10 : new_count ) ;
+        setcount(new_count);
+        setimg( 'url('+stonks+')' ) ;
+    }
+    
+    function decrease()
+    {
+        let new_count = count - 1;
+        new_count = new_count < 0 ? 0 : ( new_count > 10 ? 10 : new_count ) ;
+        setcount(new_count);
+        setimg( 'url('+notstonks+')' ) ;
+    }
+
+    return (
+        <div className="Counter_Back" ref={back} style={{backgroundImage: img }}>
+            <div className="Counter_Controls">
+            <Button onClick={decrease} color="error" variant="contained" className="Counter_Button">-</Button>
+            <div className="Counter_Label"><p>{count}</p></div>
+            <Button onClick={increase} color="success" variant="contained" className="Counter_Button">+</Button>
+            </div>
+        </div>
+    );
+
+}
